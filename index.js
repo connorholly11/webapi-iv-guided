dcd229099a1eeb812b897b2560189a793416877b
Connor Holly <connorholly11@gmail.com>
2019-10-03 12:11:21 -0500

    dynamic port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const server = require("./api/server.js");
 const defaults = require("./api/configs/defaults");
 
+console.log(defaults.port);
+
 server.listen(defaults.port, () => {
   console.log(
     `\n*** Server Running on http://localhost:${defaults.port} ***\n`
